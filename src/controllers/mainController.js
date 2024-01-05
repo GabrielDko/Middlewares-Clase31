ac0940e4d795e2fd72a08ace2600a4a2522bfb89
GabrielDko <gabriielrobless@gmail.com>
2024-01-04 22:15:21 -0300

    routes add

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,6 +3,12 @@
 const mainController = {
     index: (req,res)=>{
         res.render("index")
+    },
+    services: (req,res)=>{
+        res.send("Servicios....servicios")
+    },
+    design:(req,res)=>{
+        res.send("Diseños!")
     }
 }
 
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {index, test} = require("../controllers/mainController")
+const {index,services,design} = require("../controllers/mainController")
 
 
 router.get("/",index)
+router.get("/services",services)
+router.get("/services/design",design)
 
 module.exports = router;
